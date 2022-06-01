11e8cbe54e7616d4a9ae4ceb72d663a487866ec9
gmastrokostas <estrangedpenguin@gmail>
2022-06-01 16:09:29 -0400

    adding more info

--- examples/chmod_check.py
+++ examples/chmod_check.py
@@ -9,4 +9,22 @@ def check_x(f_path):
 f_path='/home/ansible/tmp_2'
 check_x(f_path)
 
+############################################################################
+f_path = ('test_file.txt')
+#check_file_1 = os.access(f_path, os.X_OK)
+#check_file_2 = os.access(f_path, os.F_OK)
+#check_file_3 = os.access(f_path, os.F_TEST)
+#check_file_4 = os.access(f_path, os.W_OK)
+
+status = os.stat(f_path)
+chmod_status = oct(status.st_mode)[-3:]
+
+if chmod_status != 750:
+    print("file change will not work. Changing the chmod on this file")
+    os.chmod(f_path, 0o750)
+else:
+    print("exiting")
+
+
+
 
