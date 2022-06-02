2e6104dddafeba7ae1cc98873c72422c3196da9b
gmastrokostas <estrangedpenguin@gmail>
2022-06-02 14:24:58 -0400

    get_system_info

---
+++ examples/get_system_info.py
@@ -0,0 +1,12 @@
+#USING PLATFORM MODULE
+import platform
+
+print(platform.system())
+print(platform.machine())
+print(platform.platform())
+print(platform.release())
+print(platform.version())
+print(platform.libc_ver())
+print(platform.processor())
+print(platform.uname())
+
